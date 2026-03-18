7ef875dc45f82eecb1678ab1bf34f5f880717702
abdulrehman-chaudhary <f2022266580@umt.edu.pk>
2026-03-18 17:42:02 +0500

    change

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,13 +1,10 @@
-import { NavLink, useRouteError } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 
 const ErrorPage = () => {
-  const error = useRouteError();
   return (
     <div>
       <h1>ERROR!</h1>
       <p>Cant load the page..............</p>
-      {error && <p></p>}
-      {error.data}
       <NavLink to={"/"}>
         <button> ERROR </button>
       </NavLink>
